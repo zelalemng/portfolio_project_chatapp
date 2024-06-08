42262fa09f407d707578f5dfafff3c79f932654a
zelalemng <122828485+zelalemng@users.noreply.github.com>
2024-06-08 17:14:43 +0300

    list user and chat

--- src/components/list/list.jsx
+++ src/components/list/list.jsx
@@ -1,15 +1,18 @@
+// Importing necessary libraries and components
 import React from 'react'
 import './list.css';
 import { UserInfo } from './userinfo/userInfo';
 import { ChatList } from './chatList/chatList';
 
+// List component
 export const List = () => {
     return(
         <div className='list'>
-            <UserInfo />
-            <ChatList />
+            <UserInfo /> {/* Rendering UserInfo component */}
+            <ChatList /> {/* Rendering UserInfo component */}
         </div>
     )
 }
 
+// Exporting List component as default
 export default List

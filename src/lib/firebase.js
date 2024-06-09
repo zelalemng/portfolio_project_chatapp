3843a32c3fb718934c9164b1777fde0315af9420
zelalemng <122828485+zelalemng@users.noreply.github.com>
2024-06-09 18:18:57 +0300

    firebase

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,12 +1,12 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
-import { getStorage } from "firebase/storage";
+import { initializeApp } from "firebase/app"; // Importing init app function from Firebase app
+import { getAuth } from "firebase/auth"; // Importing getAuth from Firebase auth
+import { getFirestore } from "firebase/firestore"; // Importing getFirestore function from Firebase Firestore
+import { getStorage } from "firebase/storage"; // Importing getStorage function from Firebase storage
 
-
+// Firebase configuration object containing necessary keys and configuration object
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_API_KEY,
+  apiKey: import.meta.env.VITE_API_KEY, //API key from firebase
   authDomain: "minapp-e8192.firebaseapp.com",
   projectId: "minapp-e8192",
   storageBucket: "minapp-e8192.appspot.com",
@@ -14,9 +14,14 @@ const firebaseConfig = {
   appId: "1:920797005913:web:3bc31a14004861ab560948"
 };
 
-
+// initialize firebase app with the configuration object
 const app = initializeApp(firebaseConfig);
 
+// Exporting Firebase authentication instance
 export const auth = getAuth(app)
+
+// Exporting Firestore database instance
 export const db = getFirestore(app)
+
+// Exporting Firebase storage instance
 export const storage = getStorage(app)

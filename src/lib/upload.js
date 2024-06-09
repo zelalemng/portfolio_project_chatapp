249e846a32169030fd56a0ec4e5fd3eef6542aae
zelalemng <122828485+zelalemng@users.noreply.github.com>
2024-06-09 19:27:56 +0300

    upload

--- src/lib/upload.js
+++ src/lib/upload.js
@@ -1,16 +1,20 @@
+// importing necessary fuctions from firebase storate
 import { getDownloadURL, ref, uploadBytesResumable } from "firebase/storage";
 import { storage } from "./firebase";
 
+// Function to upload a file to firebase storage
 const upload = async (file) => {
-    const date = new Date();
+    const date = new Date(); // Getting the current date
     const storageRef = ref(storage, `images/${date + file.name}`);
 
     const uploadTask = uploadBytesResumable(storageRef, file);
 
+    // Returning a promise that resolves with the download URL of the uploaded file 
     return new Promise((resolve, reject) => {
         uploadTask.on(
             "state_changed",
             (snapshot) => {
+                // calculating and logging the upload progress
                 const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
                 console.log('Upload is ' + progress +'% done');
             },
@@ -26,4 +30,5 @@ const upload = async (file) => {
     });
 
 };
+// Exporting the upload function as the default export
 export default upload;
